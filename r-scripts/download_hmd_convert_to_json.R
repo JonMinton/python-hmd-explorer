3122b960d9dd0e8204b93e83668742dab4903132
Jon Minton <jon.will.minton@gmail.com>
2023-05-08 21:23:52 +0100

    add rproject file, rscript for downloading hmd, and very big file (in .ignored dir)

---
+++ r-scripts/download_hmd_convert_to_json.R
@@ -0,0 +1,51 @@
+# install.packages("tidyverse")
+# install.packages('HMDHFDplus')
+# install.packages('jsonlite')
+# install.packages('glue')
+
+library(tidyverse)
+library(HMDHFDplus)
+library(jsonlite)
+library(glue)
+
+countryCodes <- HMDHFDplus::getHMDcountries()
+countryCodes
+
+HMDHFDplus::getHMDitemavail(countryCodes$CNTRY[1])
+
+allHmdLinks <- countryCodes %>% 
+  mutate(
+    countryDataItems = map(CNTRY, HMDHFDplus::getHMDitemavail)
+  ) %>% 
+  unnest(cols = c(countryDataItems)) %>% 
+  rename(full_link = link1)
+
+# Let's test accessing a single item via full link
+
+# testFile <- HMDHFDplus::readHMDweb(pasteallHmdLinks$full_link[4])
+
+
+allHmdDataOfInterest <- 
+  allHmdLinks %>% 
+    select(CNTRY, item)  %>% 
+    distinct() %>% 
+    filter(item %in% c(
+      "Births",
+      "Deaths_1x1",
+      "Population",
+      "Exposures_1x1",
+      "Mx_1x1",
+      "fltper_1x1",
+      "mltper_1x1",
+      "bltper_1x1",
+      "E0per",
+      "cExposures_1x1",
+      "cMx_1x1"
+    )) %>% 
+  mutate(
+    data = map2(CNTRY, item, HMDHFDplus::readHMDweb, username=username, password=password)
+  ) %>% 
+  unnest(data)
+
+
+
