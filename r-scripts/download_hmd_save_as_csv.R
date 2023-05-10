a4cae0a889a97c80ad30f82ba7bb33bd8bdf2e11
Jon Minton <jon.will.minton@gmail.com>
2023-05-10 10:55:51 +0100

    extract and save births, deaths, population, exposures, lifetables

--- r-scripts/download_hmd_save_as_csv.R
+++ r-scripts/download_hmd_save_as_csv.R
@@ -6,22 +6,23 @@
 # NOTE: To use from python consider adding the following to receive arguments 
 # for usernames and passwords
 
-args <- commandArgs(trailingOnly = TRUE)
+# args <- commandArgs(trailingOnly = TRUE)
+# 
+# username <- args[1]
+# password <- args[2]
+# 
+# print("I'm in R")
 
-username <- args[1]
-password <- args[2]
+# print(paste("username:", username))
+# print(paste("password:", password))
+library(tidyverse)
+library(HMDHFDplus)
+library(jsonlite)
+library(glue)
+library(janitor)
 
-print("I'm in R")
-
-print(paste("username:", username))
-print(paste("password:", password))
-# library(tidyverse)
-# library(HMDHFDplus)
-# library(jsonlite)
-# library(glue)
-
-# countryCodes <- HMDHFDplus::getHMDcountries()
-# countryCodes
+countryCodes <- HMDHFDplus::getHMDcountries()
+countryCodes
 
 # HMDHFDplus::getHMDitemavail(countryCodes$CNTRY[1])
 
@@ -35,6 +36,108 @@ print(paste("password:", password))
 # # Let's test accessing a single item via full link
 
 # # testFile <- HMDHFDplus::readHMDweb(pasteallHmdLinks$full_link[4])
+
+
+
+allBirths <- 
+  countryCodes %>% 
+  mutate(dta = map(CNTRY, HMDHFDplus::readHMDweb, item = 'Births', username=username, password=password)) %>% 
+  unnest(dta) %>% 
+  janitor::clean_names() %>% 
+  pivot_longer(cols = female:total, names_to = "sex", values_to = "number_of_births")
+
+write_csv(allBirths, "assets/data/births.csv")
+
+allDeaths <- 
+  countryCodes %>% 
+  mutate(dta = map(CNTRY, HMDHFDplus::readHMDweb, item = "Deaths_1x1", username=username, password=password)) %>% 
+  unnest(dta) %>% 
+  janitor::clean_names() %>% 
+  select(-open_interval) %>% 
+  pivot_longer(cols = female:total, names_to = "sex", values_to = "number_of_deaths")
+
+write_csv(allDeaths, "assets/data/deaths.csv")
+
+
+allPopulation <- 
+  countryCodes %>% 
+  mutate(dta = map(CNTRY, HMDHFDplus::readHMDweb, item = "Population", username=username, password=password)) %>% 
+  unnest(dta) %>% 
+  janitor::clean_names() %>% 
+  mutate(female = (female1 + female2) /2, male = (male1 + male2) / 2, total = (total1 + total2 ) / 2 ) %>% 
+  select(country, link, cntry, year, age, female, male, total) %>% 
+  pivot_longer(cols = female:total, names_to = "sex", values_to = "population_count")
+
+write_csv(allPopulation, "assets/data/population.csv")
+
+allExposures <- 
+  countryCodes %>% 
+  mutate(dta = map(CNTRY, HMDHFDplus::readHMDweb, item = "Exposures_1x1", username=username, password=password)) %>% 
+  unnest(dta) %>% 
+  janitor::clean_names() %>% 
+  select(country, link, cntry, year, age, female, male, total) %>% 
+  pivot_longer(cols = female:total, names_to = "sex", values_to = "exposures_count")
+
+write_csv(allExposures, "assets/data/exposures.csv")
+
+
+femaleLifetables <- 
+  countryCodes %>% 
+  mutate(dta = map(CNTRY, HMDHFDplus::readHMDweb, item = "fltper_1x1", username=username, password=password)) %>% 
+  unnest(dta)
+names(femaleLifetables) <- c("country", "link", "cntry", "year", "age", "mx", "qx", "ax", "lx", "dx", "Lx", "Tx", "ex", "openinterval")
+
+femaleLifetables <- 
+  femaleLifetables %>% 
+    select(-openinterval) %>% 
+    mutate(sex = 'female') %>% 
+    select(country, link, cntry, sex, everything())
+
+femaleLifetables
+
+
+maleLifetables <- 
+  countryCodes %>% 
+  mutate(dta = map(CNTRY, HMDHFDplus::readHMDweb, item = "mltper_1x1", username=username, password=password)) %>% 
+  unnest(dta)
+names(maleLifetables) <- c("country", "link", "cntry", "year", "age", "mx", "qx", "ax", "lx", "dx", "Lx", "Tx", "ex", "openinterval")
+
+maleLifetables <- 
+  maleLifetables %>% 
+  select(-openinterval) %>% 
+  mutate(sex = 'male') %>% 
+  select(country, link, cntry, sex, everything())
+
+maleLifetables
+
+
+bothLifetables <- 
+  countryCodes %>% 
+  mutate(dta = map(CNTRY, HMDHFDplus::readHMDweb, item = "bltper_1x1", username=username, password=password)) %>% 
+  unnest(dta)
+names(bothLifetables) <- c("country", "link", "cntry", "year", "age", "mx", "qx", "ax", "lx", "dx", "Lx", "Tx", "ex", "openinterval")
+
+bothLifetables <- 
+  bothLifetables %>% 
+    select(-openinterval) %>% 
+    mutate(sex = 'both') %>% 
+    select(country, link, cntry, sex, everything())
+
+
+allLifetables <- 
+  bothLifetables %>% 
+    bind_rows(
+      femaleLifetables
+    ) %>% 
+    bind_rows(
+      maleLifetables
+    )
+
+allLifetables
+
+write_csv(allLifetables, "assets/data/lifetables.csv")
+
+
 
 
 # allHmdDataOfInterest <- 
